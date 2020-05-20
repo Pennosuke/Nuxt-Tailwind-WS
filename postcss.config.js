d7133063029aa5f1e299769095ff2c94704d4107
Pennosuke <chinnawach_kingdom@hotmail.com>
2020-05-20 14:01:41 +0700

    edit postcss.config.js file

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project
   content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx'
+    './pages/detail/_name.vue',
+    './pages/pagination/_id.vue',
+    './pages/search/_name.vue',
+    'pages/index.vue'
     // etc.
   ],
 
